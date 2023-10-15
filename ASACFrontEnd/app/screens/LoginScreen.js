0f5b7bb624847609173584f586ed22f4bc12304a
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-10-15 23:56:49 +0100

    Configure the connection between the backend and frontend

--- ASACFrontEnd/app/screens/LoginScreen.js
+++ ASACFrontEnd/app/screens/LoginScreen.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { BACKEND_URL } from '@env';
+import { ImageBackground, View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import SharedStyles from '../styles/SharedStyles';
 
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
+    // Your logic to handle login using your backend goes here.
+    // For demonstration purposes, you can use the following lines.
     try {
       const response = await fetch(`${BACKEND_URL}/login`, {
         method: 'POST',
@@ -19,65 +21,43 @@ const LoginScreen = ({ navigation }) => {
         }),
       });
 
-      const result = await response.json();
+      const data = await response.json();
 
       if (response.ok) {
-        // Navigate to home screen or dashboard
-        Alert.alert('Success', 'Success')
-        // navigation.navigate('Home'); // Assume you have a Home screen in your stack navigator
+        // Successfully logged in
+        // Navigate or do something
       } else {
-        Alert.alert('Error', result.message || 'An error occurred');
+        Alert.alert('Login Failed', data.message || 'An error occurred');
       }
     } catch (error) {
-      Alert.alert('Error', 'Could not perform the operation.');
-      console.error(error);
+      Alert.alert('Login Failed', 'An error occurred');
     }
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        value={username}
-        onChangeText={setUsername}
-        placeholder="Username"
-        style={styles.input}
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        secureTextEntry
-        style={styles.input}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      //source={require('../../assets/PreLoginScreenBackground.png')}
+      style={SharedStyles.backgroundImage}
+      resizeMode="cover"
+    >
+      <View style={SharedStyles.container}>
+        <TextInput
+          placeholder="Username"
+          style={SharedStyles.input}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          style={SharedStyles.input}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity style={SharedStyles.button} onPress={handleLogin}>
+          <Text style={SharedStyles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    padding: 12,
-    width: 300,
-    marginBottom: 16,
-  },
-  button: {
-    backgroundColor: '#01C1DB',
-    padding: 12,
-    width: 300,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#393F43',
-    fontSize: 16,
-  },
-});
 
 export default LoginScreen;
--- ASACFrontEnd/app/screens/SignUpScreen.js
+++ ASACFrontEnd/app/screens/SignUpScreen.js
@@ -1,115 +1,92 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { BACKEND_URL } from '@env';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import SharedStyles from '../styles/SharedStyles';
 
 const SignUpScreen = ({ navigation }) => {
-    const [username, setUsername] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [username, setUsername] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirmation, setPasswordConfirmation] = useState();
 
-    const handleSignUp = async () => {
-        const response = await fetch(`${BACKEND_URL}/signup`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                first_name: firstName,
-                last_name: lastName,
-                email,
-                password,
-                password_confirmation: passwordConfirmation,
-            }),
-        });
+  const handleSignUp = async () => {
+    // Your logic to handle sign up using your backend goes here.
+    // For demonstration purposes, you can use the following lines.
+    if (password !== passwordConfirmation) {
+      Alert.alert('Error', 'Passwords do not match');
+      return;
+    }
 
-        const data = await response.json();
+    try {
+      const response = await fetch(`${BACKEND_URL}/sign_up/`, {
+        method: 'POST',
+        headers: {
+          Accept: "application/json",
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          username: username,
+          email: email,
+          new_password: password,
+          password_confirmation: passwordConfirmation,
+        }),
+      });
 
-        if (response.ok) {
-            Alert.alert('Success', 'You have successfully signed up.');
-            // Navigate to login or another part of your app
-        } else {
-            Alert.alert('Error', data.message || 'An error occurred.');
-        }
-    };
+      const data = await response.json();
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                placeholder="Username"
-                value={username}
-                onChangeText={setUsername}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="First Name"
-                value={firstName}
-                onChangeText={setFirstName}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Last Name"
-                value={lastName}
-                onChangeText={setLastName}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Password"
-                value={password}
-                onChangeText={setPassword}
-                style={styles.input}
-                secureTextEntry
-            />
-            <TextInput
-                placeholder="Confirm Password"
-                value={passwordConfirmation}
-                onChangeText={setPasswordConfirmation}
-                style={styles.input}
-                secureTextEntry
-            />
-            <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-                <Text style={styles.buttonText}>Sign Up</Text>
-            </TouchableOpacity>
-        </View>
-    );
+      if (response.ok) {
+        // Successfully signed up
+        // Navigate or do something
+      } else {
+        Alert.alert('Sign Up Failed', data.message || 'An error occurred');
+      }
+    } catch (error) {
+      Alert.alert('Sign Up Failed', 'An error occurred');
+    }
+  };
+
+  return (
+    <View style={SharedStyles.container}>
+      <TextInput
+        placeholder="Username"
+        style={SharedStyles.input}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="First Name"
+        style={SharedStyles.input}
+        onChangeText={setFirstName}
+      />
+      <TextInput
+        placeholder="Last Name"
+        style={SharedStyles.input}
+        onChangeText={setLastName}
+      />
+      <TextInput
+        placeholder="Email"
+        style={SharedStyles.input}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        style={SharedStyles.input}
+        onChangeText={setPassword}
+      />
+      <TextInput
+        placeholder="Confirm Password"
+        secureTextEntry
+        style={SharedStyles.input}
+        onChangeText={setPasswordConfirmation}
+      />
+      <TouchableOpacity style={SharedStyles.button} onPress={handleSignUp}>
+        <Text style={SharedStyles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    input: {
-        width: '80%',
-        height: 44,
-        padding: 10,
-        marginBottom: 10,
-        backgroundColor: 'rgba(220, 220, 220, 0.8)',
-    },
-    button: {
-        height: 44,
-        width: '80%', // Use 80% width to align with the TextInput fields
-        backgroundColor: 'rgba(1, 193, 219, 0.8)', // Semi-transparent background color
-        marginBottom: 16,
-        borderRadius: 10, // Rounded corners
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: 'rgb(57, 63, 67)',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-});
 
 export default SignUpScreen;
