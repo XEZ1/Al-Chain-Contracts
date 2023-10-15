16bb8f1557e1e5bcf2d8b5f6122d56eb674dbd8e
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-10-15 21:19:45 +0100

    Add a model of accounts

--- ASACBackEnd/Accounts/models.py
+++ ASACBackEnd/Accounts/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    """User model used for authentication."""
+
+    username = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^(?=.*[a-zA-Z]{3,}).*$',
+            message='Username must contain at least 3 alphabetical characters'
+        )]
+    )
+    first_name = models.CharField(
+        max_length=15,
+        blank=False
+    )
+    last_name = models.CharField(
+        max_length=15,
+        blank=False
+    )
+    email = models.EmailField(
+        unique=True,
+        max_length=35,
+        blank=False
+    )
+    paying_user = models.BooleanField(
+        default=False
+    )
