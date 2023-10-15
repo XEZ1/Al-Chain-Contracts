b7a68c7ca1903c85461e1244d1bdb84d0b0985d4
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-10-15 21:26:31 +0100

    Configure views

--- ASACBackEnd/Accounts/views.py
+++ ASACBackEnd/Accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SignUpSerialiser, UserSerialiser
+from Accounts.models import User
+from rest_framework import generics, permissions
+
 
 # Create your views here.
+# Token validation for the user logged in.
+@api_view(['GET'])
+def validate_token(request):
+    user = User.objects.get(username=request.user.username)
+    if user:
+        return Response({'token_valid': True})
+    return Response({'token_valid': False})
+
+
+class SignUp(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SignUpSerialiser
+    queryset = User.objects.all()
+
+    def get(self, request):
+        return Response({'detail': 'GET method not allowed.'})
