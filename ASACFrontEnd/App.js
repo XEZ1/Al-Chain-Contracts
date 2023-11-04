cec5c47c97b3de587a9fc9cc5c4689f04f8239c6
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-11-04 15:03:04 +0000

    Create notifications and redefine them

--- ASACFrontEnd/App.js
+++ ASACFrontEnd/App.js
@@ -3,9 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './app/components/Authentication';
 import AppNavigator from './app/navigation/AppNavigator';
 import { ThemeProvider } from './app/components/Theme';
+import { useNotification } from './app/components/Notifications';
 
 
 const App = () => {
+    useNotification();
     return (
         <AuthProvider>
             <ThemeProvider>
