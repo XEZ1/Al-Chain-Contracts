d2b173e00f19ca2cfd23220b7d14d478d7fb6367
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-11-07 22:22:17 +0000

    Add a supportchatscreen and icons

---
+++ ASACFrontEnd/app/screens/PostLoginScreens/SupportScreen.js
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+import { View, Text, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import getStyles from '../../styles/SharedStyles';
+import { ThemeContext } from '../../components/Theme';
+
+const SupportScreen = ({ navigation }) => {
+    const { theme } = useContext(ThemeContext);
+    const styles = getStyles(theme); // Applying the theme to the styles
+    const [message, setMessage] = useState('');
+
+    // Dummy data for messages
+    const messages = [
+        { id: '1', text: 'Hi! How can I help you today?', isAssistant: true },
+        // ... other messages
+    ];
+
+    // Handler for sending a message
+    const handleSendMessage = () => {
+        if (message.trim()) {
+            // Here, you'd normally append the message to your messages state or send it to your backend
+            console.log('Sending message:', message);
+            setMessage(''); // Clear the input field after sending the message
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Support Chat</Text>
+
+            <ScrollView
+                style={{ flex: 1, width: '100%' }}
+                contentContainerStyle={{ padding: 10 }}
+            >
+                {/* Message bubbles */}
+                {messages.map((msg) => (
+                    <View
+                        key={msg.id}
+                        style={[
+                            styles.card,
+                            {
+                                alignSelf: msg.isAssistant ? 'flex-start' : 'flex-end',
+                                backgroundColor: msg.isAssistant ? styles.card.backgroundColor : styles.tabBar.activeTintColor,
+                            },
+                        ]}
+                    >
+                        <Text style={[styles.settingText, { color: theme === 'dark' ? '#FFF' : '#000' }]}>
+                            {msg.text}
+                        </Text>
+                    </View>
+                ))}
+            </ScrollView>
+
+            {/* Input area */}
+            <View style={{ flexDirection: 'row', alignItems: 'center', paddingVertical: 10 }}>
+                <TextInput
+                    value={message}
+                    onChangeText={setMessage}
+                    placeholder='Type your message here...'
+                    placeholderTextColor={theme === 'dark' ? 'grey' : 'darkgrey'}
+                    style={[
+                        styles.input,
+                        {
+                            flex: 1,
+                            marginRight: 10,
+                            marginTop: 16,
+                            height: 50, // Align height with button
+                        }
+                    ]}
+                    multiline
+                />
+                <TouchableOpacity onPress={handleSendMessage} style={[styles.sendButton, { height: 50, paddingHorizontal: 20 }]}>
+                    <Text style={styles.buttonText}>Send</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+export default SupportScreen;
