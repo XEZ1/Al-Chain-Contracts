4e2179b778602e22620147339f68154f0a65f807
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-01-24 20:37:54 +0000

    Write a model nad views for notifications + Redefine the norifications handling in the frontend

--- ASACBackEnd/Notifications/models.py
+++ ASACBackEnd/Notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class PushToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255, unique=True)
