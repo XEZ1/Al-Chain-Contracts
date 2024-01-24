bb116bedd8b964c1af422abccdbf57b10f2dcd3f
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-01-24 20:19:06 +0000

    Route the consumer

--- ASACBackEnd/Notifications/routing.py
+++ ASACBackEnd/Notifications/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import consumers  # Import your consumers here
+from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/some_path/', consumers.YourConsumer.as_asgi()),  # Example route
+    path('ws/notifications/', consumers.NotificationConsumer.as_asgi()),
 ]
