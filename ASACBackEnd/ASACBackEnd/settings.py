61bf90e44df3ab003d2c7b1e6942219ab01ec6bc
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-03 16:41:27 +0000

    Update the frontend base of the home screen for connection to the AI model + Add .expo/ to .gitignore

--- ASACBackEnd/ASACBackEnd/settings.py
+++ ASACBackEnd/ASACBackEnd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sd*6c$qhzhfw7k#ncii@3nnzxco@k&+n%fq0_=ze5hg7+j9k(z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.18', '192.168.0.14', 'b8c3-82-8-95-71.ngrok-free.app']
+ALLOWED_HOSTS = ['192.168.0.18', '192.168.0.14']
 
 ###
 # Path to your service account key file
