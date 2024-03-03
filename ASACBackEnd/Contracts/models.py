96e78d3f843c1f4d52fc4c38ceeaca899695a1c3
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-03 17:38:05 +0000

    Add a model for smart contracts

--- ASACBackEnd/Contracts/models.py
+++ ASACBackEnd/Contracts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class SmartContract(models.Model):
+    SALARY_TYPE_CHOICES = [
+        ('monthly', 'Monthly'),
+        ('bi-weekly', 'Bi-Weekly'),
+        ('weekly', 'Weekly'),
+    ]
+
+    code = models.TextField()
+    employer_usdc_address = models.CharField(max_length=42)
+    employee_usdc_address = models.CharField(max_length=42)
+    authorized_app_usdc_address = models.CharField(max_length=42)
+    monthly_salary = models.DecimalField(max_digits=20, decimal_places=5)
+    usdc_token_contract_interface = models.TextField()
+    start_date = models.BigIntegerField()
+    termination_date = models.BigIntegerField()
+    performance_score = models.IntegerField()
+    performance_threshold = models.IntegerField()
+    is_employed = models.BooleanField(default=True)
+    salary_type = models.CharField(max_length=10, choices=SALARY_TYPE_CHOICES)
+
+    def __str__(self):
+        return f"Smart Contract {self.id} - {self.employer_usdc_address} to {self.employee_usdc_address}"
+
