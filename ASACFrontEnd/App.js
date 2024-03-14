d461464bbcd99f94fdcb36a6c1c310d1ab82a1aa
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-14 00:37:42 +0000

    Add a hook for notifications in the frontend

--- ASACFrontEnd/App.js
+++ ASACFrontEnd/App.js
@@ -5,9 +5,18 @@ import AppNavigator from './app/navigation/AppNavigator';
 import { ThemeProvider } from './app/components/Theme';
 import { connectToNotifications } from './app/components/Notifications';
 import { BACKEND_URL } from '@env';
+import * as Notifications from 'expo-notifications';
 
 
 const App = () => {
+    useEffect(() => {
+        const subscription = Notifications.addNotificationReceivedListener(notification => {
+            console.log(notification);
+        });
+
+        return () => Notifications.removeNotificationSubscription(subscription);
+    }, []);
+
     return (
         <AuthProvider>
             <ThemeProvider>
