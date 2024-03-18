40472337ab3d2783d60a94ff040b9532533cb6f4
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-18 00:11:50 +0000

    Apply the data validation on models

--- ASACBackEnd/Contracts/models.py
+++ ASACBackEnd/Contracts/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from Accounts.models import User
+from django_ethereum.fields import EthereumAddressField
 from .validators import (validate_ethereum_address, validate_hexadecimal,
                          validate_contract_name, validate_json_format)
 
 
 class EmploymentContract(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    contract_name = models.CharField(max_length=255)
-    employer_address = models.CharField(max_length=42)
-    auth_app_address = models.CharField(max_length=42)
-    token_contract_interface = models.TextField()
+    contract_name = models.CharField(max_length=255, validators=[validate_contract_name])
+    employer_address = models.CharField(max_length=42, validators=[validate_ethereum_address, validate_hexadecimal])
+    auth_app_address = models.CharField(max_length=42, validators=[validate_ethereum_address, validate_hexadecimal])
+    token_contract_interface = models.TextField(max_length=42, validators=[validate_ethereum_address, validate_hexadecimal])
     contract_content = models.TextField()
 
     def __str__(self):
@@ -20,10 +21,10 @@ class SmartContract(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     legal_contract = models.ForeignKey(EmploymentContract, on_delete=models.CASCADE, related_name='smart_contracts')
     code = models.TextField()
-    contract_name = models.CharField(max_length=255)
-    employer_address = models.CharField(max_length=42)
-    auth_app_address = models.CharField(max_length=42)
-    token_contract_interface = models.TextField()
+    contract_name = models.CharField(max_length=255, validators=[validate_contract_name])
+    employer_address = models.CharField(max_length=42, validators=[validate_ethereum_address, validate_hexadecimal])
+    auth_app_address = models.CharField(max_length=42, validators=[validate_ethereum_address, validate_hexadecimal])
+    token_contract_interface = models.TextField(validators=[validate_ethereum_address, validate_hexadecimal])
 
     def __str__(self):
         return f"Smart Contract {self.id} - {self.contract_name}"
