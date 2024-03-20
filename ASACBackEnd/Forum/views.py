beaecd6c707ea62ba4bb947c9e632b72cf83ad48
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-20 02:44:40 +0000

    Create views for posts, likes and comments

--- ASACBackEnd/Forum/views.py
+++ ASACBackEnd/Forum/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# forum/views.py
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post, Comment, Like
+from .serializers import PostSerialiser, CommentSerialiser, LikeSerialiser
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerialiser
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerialiser
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CommentCreate(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerialiser
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class LikeCreateDelete(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        like, created = Like.objects.get_or_create(post=post, user=request.user)
+        if created:
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        like = get_object_or_404(Like, post=post, user=request.user)
+        like.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
