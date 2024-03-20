9f86d6fac4b3c70ef94fbc9029339361d9d41344
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-20 02:38:15 +0000

    Create an app for Forums; Add a simple model for posts

---
+++ ASACBackEnd/Forum/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from Accounts.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forum_posts')
+
+    def __str__(self):
+        return self.title
+
--- ASACFrontEnd/app/screens/PostLoginScreens/Home/UseEditor.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import { Alert, LayoutAnimation } from 'react-native';
-import * as DocumentPicker from 'expo-document-picker';
-import * as SecureStore from 'expo-secure-store';
-import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing';
-import { BACKEND_URL } from '@env';
-
