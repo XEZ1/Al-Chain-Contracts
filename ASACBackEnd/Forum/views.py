e7937a4febf9c91ce7df3e91b38e18184b7ba548
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-20 02:45:35 +0000

    Create urls for the newly defined forum

--- ASACBackEnd/Forum/views.py
+++ ASACBackEnd/Forum/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-
-
-# forum/views.py
-
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
