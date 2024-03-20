da2df2a2f2ea845f29040b656567712428d40ffb
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-20 02:59:01 +0000

    Implement UI calls to the backend for forum app

---
+++ ASACFrontEnd/app/screens/PostLoginScreens/Forum/UseForumScreen.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import { BACKEND_URL } from '@env';
+
+
+export const useForumScreen = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const fetchPosts = async () => {
+        setLoading(true);
+        try {
+            const token = await SecureStore.getItemAsync('authToken');
+            const response = await fetch(`${BACKEND_URL}/forum/posts/`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json(); 
+            setPosts(data);
+        } catch (error) {
+            console.error('Failed to fetch posts:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const createPost = async (title, description) => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/forum/posts/`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ title, description }),
+            });
+            if (response.ok) {
+                const newPost = await response.json();
+                setPosts(currentPosts => [newPost, ...currentPosts]); 
+            } else {
+                console.error('Failed to create post');
+            }
+        } catch (error) {
+            console.error('Failed to create post:', error);
+        }
+    };
+
+    return { posts, loading, createPost };
+};
