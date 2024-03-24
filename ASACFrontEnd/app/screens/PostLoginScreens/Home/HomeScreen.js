d2a48a476657492b39c4588e3d8d380e73a646a6
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-24 01:04:24 +0000

    Adjust the padding

--- ASACFrontEnd/app/screens/PostLoginScreens/Home/HomeScreen.js
+++ ASACFrontEnd/app/screens/PostLoginScreens/Home/HomeScreen.js
@@ -65,7 +65,7 @@ const HomeScreen = ({ navigation }) => {
                     const nodeHandle = findNodeHandle(currentlyFocusedField);
                     scrollViewRef.current?.getScrollResponder().scrollResponderScrollNativeHandleToKeyboard(
                         nodeHandle,
-                        120,
+                        160,
                         true
                     );
                 }
