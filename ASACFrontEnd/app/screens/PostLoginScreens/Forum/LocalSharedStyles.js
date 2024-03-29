6a79eb52c15730c350cfb878fa656687da733539
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-29 22:47:21 +0000

    Roll to a local styling for commentsscreen

--- ASACFrontEnd/app/screens/PostLoginScreens/Forum/LocalSharedStyles.js
+++ ASACFrontEnd/app/screens/PostLoginScreens/Forum/LocalSharedStyles.js
@@ -22,7 +22,73 @@ const getLocalStyles = (theme = 'light') => {
     };
 
     return StyleSheet.create({
-        
+        containerCommentScreen: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: '0%',
+            padding: '5%',
+            backgroundColor: themeStyles[theme].containerBackground, 
+        },
+        flatListCommentsContainer: {
+            width: '100%',
+            marginBottom: 90,
+        },
+        regularPadding: {
+            paddingTop: 20,
+            paddingBottom: 5,
+        },
+        settingText: {
+            fontSize: 18,
+            color: themeStyles[theme].textColor, 
+        },
+        postsContainer: {
+            flexDirection: 'row',
+            marginTop: 10,
+            alignItems: 'center', 
+            justifyContent: 'space-between', 
+        },
+        postsButtonText: {
+            flexDirection: 'row', 
+            alignItems: 'center', 
+            marginRight: 0,
+        },
+        buttonText: {
+            color: themeStyles[theme].textColor,
+            fontSize: 16,
+            fontWeight: 'bold',
+        },
+        centeredContainer: {
+            flex: 1, 
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '2%', 
+        },
+        inputCommentsScreen: {
+            height: 44,
+            width: '96%', 
+            backgroundColor: themeStyles[theme].inputBackground,
+            borderColor: themeStyles[theme].borderColor,
+            borderWidth: 1,
+            borderRadius: 10,
+            marginBottom: '5.5%',
+            paddingLeft: '4%',
+            color: themeStyles[theme].textColor, 
+        },
+        buttonCommentsScreen: {
+            height: 44,
+            width: '96%',
+            backgroundColor: themeStyles[theme].backgroundColor,
+            marginBottom: '5.7%',
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        settingText: {
+            fontSize: 18,
+            color: themeStyles[theme].textColor, 
+        },
+     
     });
 };
 
