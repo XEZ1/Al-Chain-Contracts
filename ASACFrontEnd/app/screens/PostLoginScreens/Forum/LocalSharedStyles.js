baff25950ce48f9a58de6474ff9877897d01c4b9
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-03-29 22:35:44 +0000

    Create a base for rolling to local styles for forum screens

---
+++ ASACFrontEnd/app/screens/PostLoginScreens/Forum/LocalSharedStyles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+const getLocalStyles = (theme = 'light') => {
+    const themeStyles = {
+        light: {
+            backgroundColor: 'rgba(1, 193, 219, 1)',
+            textColor: 'rgb(57, 63, 67)',
+            inputBackground: 'white',
+            borderColor: 'rgba(0, 0, 0, 0.5)',
+            containerBackground: 'white',
+            cardBackground: 'white',
+        },
+        dark: {
+            backgroundColor: 'rgba(1, 193, 219, 1)',
+            textColor: 'rgb(255, 255, 255)',
+            inputBackground: 'rgb(28, 28, 30)',
+            borderColor: 'rgba(255, 255, 255, 0.5)',
+            containerBackground: 'rgb(0, 0, 0)',
+            containerBackground: '#1A1A1A',
+            cardBackground: 'rgb(50, 50, 52)',
+        },
+    };
+
+    return StyleSheet.create({
+        
+    });
+};
+
+export default getLocalStyles;
