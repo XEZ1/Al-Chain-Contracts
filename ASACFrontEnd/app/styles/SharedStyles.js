d7e2b0a6783732c1cefb8a9e871f0e7893ae7ef6
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-04-06 01:55:45 +0100

    Adjust the styling files to use the new separate themeStyles file

--- ASACFrontEnd/app/styles/SharedStyles.js
+++ ASACFrontEnd/app/styles/SharedStyles.js
@@ -1,29 +1,7 @@
 import { StyleSheet } from 'react-native';
+import themeStyles from './ThemeStyles';
 
 const getStyles = (theme = 'light') => {
-    const themeStyles = {
-        light: {
-            backgroundColor: 'rgba(1, 193, 219, 1)',
-            textColor: 'rgb(57, 63, 67)', 
-            inputBackground: 'white',
-            borderColor: 'rgba(0, 0, 0, 0.5)',
-            containerBackground: 'white', 
-            cardBackground: 'white', 
-            shadowColor: '#000',
-            shadowOpacity: '0.4%',
-        },
-        dark: {
-            backgroundColor: 'rgba(1, 193, 219, 1)', 
-            textColor: 'rgb(255, 255, 255)', 
-            inputBackground: 'rgb(28, 28, 30)', 
-            borderColor: 'rgba(255, 255, 255, 0.5)', 
-            containerBackground: '#1A1A1A',
-            cardBackground: 'rgb(50, 50, 52)',  
-            shadowColor:'#3A3A3A',
-            shadowOpacity: '0.8%',
-        },
-    };
-
     return StyleSheet.create({
         tabBar: { //tab bar styling
             activeTintColor: theme === 'dark' ? 'white' : 'black', 
