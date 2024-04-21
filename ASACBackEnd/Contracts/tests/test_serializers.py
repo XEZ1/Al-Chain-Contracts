c66069271b660ff6bc5839810d38fdc4cf0edad8
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-04-21 22:42:08 +0100

    Test contracts' validators

--- ASACBackEnd/Contracts/tests/test_serializers.py
+++ ASACBackEnd/Contracts/tests/test_serializers.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from rest_framework import status
 from ..models import SmartContract, EmploymentContract
 from Accounts.models import User
 from ..serializers import SmartContractSerialiser, EmploymentContractSerialiser
