124f354987dd034437b7c1d80f7ab76a0191afb0
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-03 21:22:33 +0100

    Test forums' apps

---
+++ ASACBackEnd/Forums/tests/test_apps.py
@@ -0,0 +1,9 @@
+from django.apps import apps
+from django.test import TestCase
+from Contracts.apps import ContractsConfig
+
+
+class ContractsConfigTest(TestCase):
+    def test_app_config(self):
+        self.assertEqual(ContractsConfig.name, 'Forums')
+        self.assertEqual(apps.get_app_config('Forums').name, 'Forums')
