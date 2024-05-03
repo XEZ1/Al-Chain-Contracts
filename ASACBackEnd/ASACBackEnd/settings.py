6e7611606ab03cd798f6101a9615f54d2c16d201
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-03 22:58:36 +0100

    Migrate to posgresql

--- ASACBackEnd/ASACBackEnd/settings.py
+++ ASACBackEnd/ASACBackEnd/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import sys
+import sys
 from pathlib import Path
 import channels
 from corsheaders.defaults import default_headers
@@ -125,10 +126,21 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'asacbackenddb',
+        'USER': 'xez1',
+        'PASSWORD': 'g9hgim5A!!!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
