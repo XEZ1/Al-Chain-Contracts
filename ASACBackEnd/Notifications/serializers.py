38eaf85505f0ab7fea92581f4ce68582bdac17cd
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-04 15:31:52 +0100

    Test notifications' serialisers

--- ASACBackEnd/Notifications/serializers.py
+++ ASACBackEnd/Notifications/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from .models import NotificationPushToken
 
 
-class AuthenticationPushTokenSerialiser(serializers.ModelSerializer):
+class NotificationPushTokenSerialiser(serializers.ModelSerializer):
     class Meta:
         model = NotificationPushToken
         fields = ('token', 'created_at')
-        read_only_fields = ('created_at',)  # 'created_at' should not be editable
+        read_only_fields = ('created_at',)
 
     def create(self, validated_data):
         user = self.context['request'].user
