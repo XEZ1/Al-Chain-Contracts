5396ebc8e49bd86bc105ed32c1dc50591926530a
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-08 00:32:29 +0100

    Test forums' views

--- ASACBackEnd/Forums/views.py
+++ ASACBackEnd/Forums/views.py
@@ -13,15 +13,15 @@ class PostListCreateView(APIView):
 
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
-        serializer = PostSerialiser(posts, many=True, context={'request': request})
-        return Response(serializer.data)
+        serialiser = PostSerialiser(posts, many=True, context={'request': request})
+        return Response(serialiser.data)
 
     def post(self, request, *args, **kwargs):
-        serializer = PostSerialiser(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serialiser = PostSerialiser(data=request.data, context={'request': request})
+        if serialiser.is_valid():
+            serialiser.save(author=request.user)
+            return Response(serialiser.data, status=status.HTTP_201_CREATED)
+        return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostDetailView(APIView):
@@ -29,16 +29,18 @@ class PostDetailView(APIView):
 
     def get(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
-        serializer = PostSerialiser(post, context={'request': request})
-        return Response(serializer.data)
+        serialiser = PostSerialiser(post, context={'request': request})
+        return Response(serialiser.data)
 
     def put(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
-        serializer = PostSerialiser(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if post.author != request.user:
+            return Response({'detail': 'You do not have permission to edit this post.'}, status=status.HTTP_403_FORBIDDEN)
+        serialiser = PostSerialiser(post, data=request.data, context={'request': request})
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response(serialiser.data)
+        return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
@@ -52,13 +54,15 @@ class CommentCreateView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def post(self, request, pk, *args, **kwargs):
-        request.data['post'] = pk
-        request.data['author'] = request.user.id
-        serializer = CommentSerialiser(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Make a mutable copy of the data
+        data = request.data.copy()
+        data['post'] = pk
+        data['author'] = request.user.id
+        serialiser = CommentSerialiser(data=data)
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response(serialiser.data, status=status.HTTP_201_CREATED)
+        return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentListView(APIView):
@@ -67,8 +71,8 @@ class CommentListView(APIView):
     def get(self, request, pk, *args, **kwargs):
         comments = Comment.objects.filter(post=pk).order_by(
             '-created_at')
-        serializer = CommentSerialiser(comments, many=True, context={'request': request})
-        return Response(serializer.data)
+        serialiser = CommentSerialiser(comments, many=True, context={'request': request})
+        return Response(serialiser.data)
 
 
 class LikeCreateDeleteView(APIView):
@@ -78,16 +82,14 @@ class LikeCreateDeleteView(APIView):
         post = get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(post=post, user=request.user)
         if created:
-            serializer = LikeSerialiser(like)
+            serialiser = LikeSerialiser(like)
             self.notify_post_owner(post, request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        elif not created:
+            return Response(serialiser.data, status=status.HTTP_201_CREATED)
+        else:
             like.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({"detail": "Like already exists."}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, *args, **kwargs):
-        print("inside")
         post = get_object_or_404(Post, pk=pk)
         try:
             like = Like.objects.get(post=post, user=request.user)
