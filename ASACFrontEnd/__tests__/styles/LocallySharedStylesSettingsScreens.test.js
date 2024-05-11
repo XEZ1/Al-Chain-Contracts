74082105afa88cf4d86b1a0d0c59d96a7e6952a0
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-11 20:37:29 +0100

    test locallysharedstylessettingstscreen

--- ASACFrontEnd/__tests__/styles/LocallySharedStylesSettingsScreens.test.js
+++ ASACFrontEnd/__tests__/styles/LocallySharedStylesSettingsScreens.test.js
@@ -1,6 +1,36 @@
-describe('Placeholder test suite', () => {
-    it('should always pass', () => {
-      expect(true).toBeTruthy();
+import { StyleSheet } from 'react-native';
+import getLocallySharedStylesSettingsScreen from '../../app/styles/LocallySharedStylesSettingsScreens';
+
+describe('getLocallySharedStylesSupportScreens', () => {
+    beforeEach(() => {
+        StyleSheet.create = jest.fn(styles => styles);
     });
-  });
-  
+
+    it('should create styles correctly when theme is not provided', () => {
+        const styles = getLocallySharedStylesSettingsScreen();
+        expect(StyleSheet.create).toHaveBeenCalled();
+        expect(styles).toEqual(expect.any(Object));
+    });
+
+    it('should create styles correctly for light theme', () => {
+        const styles = getLocallySharedStylesSettingsScreen('light');
+        expect(StyleSheet.create).toHaveBeenCalled();
+        expect(styles).toEqual({
+            maxWidthSmallMarginBottom: {
+                width: '100%',
+                marginBottom: '3%',
+            },
+        });
+    });
+
+    it('should create styles correctly for dark theme', () => {
+        const styles = getLocallySharedStylesSettingsScreen('dark');
+        expect(StyleSheet.create).toHaveBeenCalled();
+        expect(styles).toEqual({
+            maxWidthSmallMarginBottom: {
+                width: '100%',
+                marginBottom: '3%',
+            },
+        });
+    });
+});
