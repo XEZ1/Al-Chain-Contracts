4f2da668131c41adb51b56f3a35bca6f2454cdf7
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-05-12 15:30:47 +0100

    Unsuccesfully test appnavigator

--- ASACFrontEnd/__tests__/navigation/AppNavigator.test.js
+++ ASACFrontEnd/__tests__/navigation/AppNavigator.test.js
@@ -1,3 +1,63 @@
+//import React from 'react';
+//import { render } from '@testing-library/react-native';
+//import AppNavigator from '../../app/navigation/AppNavigator';
+//import { AuthContext } from '../../app/components/Authentication';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { Text } from 'react-native';
+//
+//
+//
+//jest.mock('@react-navigation/bottom-tabs', () => {
+//    const actualNav = jest.requireActual('@react-navigation/bottom-tabs');
+//    return {
+//        createBottomTabNavigator: () => actualNav.createBottomTabNavigator(),
+//    };
+//});
+//
+//jest.mock('@react-navigation/stack', () => {
+//    const actualStack = jest.requireActual('@react-navigation/stack');
+//    return {
+//        createStackNavigator: () => actualStack.createStackNavigator(),
+//    };
+//});
+//
+//jest.mock('../../app/components/Theme', () => ({
+//    ThemeContext: {
+//        Provider: ({children}) => <div>{children}</div>,
+//        Consumer: ({children}) => children({ theme: 'light' }),
+//    }
+//}));
+//
+//jest.mock('../../app/navigation/PostLoginTabs', () => (props) => <div {...props}>PostLoginTabs</div>);
+//
+//jest.mock('../../app/navigation/PreLoginStack', () => (props) => <div {...props}>PreLoginStack</div>);
+//
+//describe('AppNavigator', () => {
+//    const setIsLoggedIn = jest.fn();
+//
+//    const renderAppNavigator = (isLoggedIn) => {
+//        return render(
+//            <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+//                <NavigationContainer>
+//                    <AppNavigator />
+//                </NavigationContainer>
+//            </AuthContext.Provider>
+//        );
+//    };
+//
+//    it('renders PostLoginTabs when isLoggedIn is true', async () => {
+//        const { findByTestId, debug } = renderAppNavigator(true);
+//        debug();
+//        expect(await findByTestId('postLoginTabsTestID')).not.toBeNull();
+//    });
+//
+//    it('renders PreLoginStack when isLoggedIn is false', async () => {
+//        const { findByTestId, debug } = renderAppNavigator(false);
+//        debug();
+//        expect(await findByTestId('preLoginStackTestID')).not.toBeNull();
+//    });
+//});
+
 describe('Placeholder test suite', () => {
     it('should always pass', () => {
         expect(true).toBeTruthy();
