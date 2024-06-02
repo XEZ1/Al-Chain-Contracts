770e88891eafca3cca3c4afcfe4d57e4cb51ae00
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-06-02 19:48:46 +0100

    Test errorboundary

--- ASACFrontEnd/ErrorBoundary.js
+++ ASACFrontEnd/ErrorBoundary.js
@@ -13,13 +13,11 @@ class ErrorBoundary extends React.Component {
     }
 
     componentDidCatch(error, errorInfo) {
-        // You can also log the error to an error reporting service
         console.log(error, errorInfo);
     }
 
     render() {
         if (this.state.hasError) {
-            // You can render any custom fallback UI
             return <Text>Something went wrong.</Text>;
         }
 
