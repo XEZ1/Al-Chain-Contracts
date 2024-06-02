a3760e302cf518db168b4c745d1eb59fac7bfd80
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-06-02 19:37:32 +0100

    test babel.config

--- ASACFrontEnd/__tests__/babel.config.test.js
+++ ASACFrontEnd/__tests__/babel.config.test.js
@@ -1,3 +1,57 @@
+describe('Babel configuration', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+
+    function loadConfig(envSetting) {
+		const mockApi = { cache: () => {} }; 
+        return require('../babel.config.js')(mockApi, envSetting);
+    }
+
+	it('should return the correct configuration for the default environment', () => {
+        const config = require('../babel.config.js')({ cache: () => {} });
+        expect(config).toEqual({
+            presets: [
+                'babel-preset-expo',
+                '@babel/preset-typescript'
+            ],
+            plugins: [
+                ['module:react-native-dotenv', {
+                    "moduleName": "@env",
+                    "path": ".env",
+                }]
+            ]
+        });
+    });
+
+    it('should return the correct configuration for test environment', () => {
+        const config = loadConfig('test');
+        expect(config).toEqual({
+            presets: [
+                'babel-preset-expo',
+                '@babel/preset-typescript'
+            ],
+            plugins: [
+                ['module:react-native-dotenv', {
+                    "moduleName": "@env",
+                    "path": ".env",
+                }]
+            ]
+        });
+    });
+
+    it('should return the correct configuration for non-test environment', () => {
+		const config = loadConfig('production');
+        expect(config).toEqual({
+            presets: ['babel-preset-expo'],
+            plugins: [
+                ['module:react-native-dotenv']
+            ]
+        });
+    });
+});
+
+
 describe('Placeholder test suite', () => {
 	it('should always pass', () => {
 		expect(true).toBeTruthy();
