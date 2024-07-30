4b1796d22e3a26eb81674da4248f9d7edd6708d4
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2024-07-30 19:37:39 +0100

    Add tests for home screen for the new features

--- ASACFrontEnd/app/screens/PostLoginScreens/Home/HomeScreen.js
+++ ASACFrontEnd/app/screens/PostLoginScreens/Home/HomeScreen.js
@@ -54,7 +54,7 @@ const HomeScreen = ({ navigation }) => {
 
     return (
         <View style={[localStyles.backgroundContainer, { paddingBottom: keyboardHeight }]}>
-            <ScrollView ref={scrollViewRef} style={keyboardHeight > 0 && Platform.OS === 'android' ? sharedStyles.avoidingTabBarContainer : sharedStyles.mediumMarginBottom } showsVerticalScrollIndicator={false}>
+            <ScrollView ref={scrollViewRef} style={keyboardHeight > 0 && Platform.OS === 'android' ? sharedStyles.avoidingTabBarContainer : sharedStyles.mediumMarginBottom } showsVerticalScrollIndicator={false} testID='scrollViewTestID'>
                 <View style={sharedStyles.container}>
                     <Text style={sharedStyles.pageHeaderText}>Smart Contract Toolkit</Text>
 
@@ -241,7 +241,7 @@ const HomeScreen = ({ navigation }) => {
             </ScrollView>
 
             {/* Separator Line */}
-            <View style={[sharedStyles.separatorLine, { bottom:  keyboardHeight > 0 && Platform.OS === 'android' ? 0 : keyboardHeight + 90  }]} />
+            <View style={[sharedStyles.separatorLine, { bottom:  keyboardHeight > 0 && Platform.OS === 'android' ? 0 : keyboardHeight + 90  }]} testID='separatorLineTestID'/>
 
         </View >
     );
